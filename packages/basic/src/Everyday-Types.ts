0fad264684ac705de30b72f800c543ca35bd5f87
weiyafei <2553241022@qq.com>
2023-01-17 21:55:12 +0800

    feat: union

--- packages/basic/src/Everyday-Types.ts
+++ packages/basic/src/Everyday-Types.ts
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: wsy
  * @Date: 2023-01-16 12:59:55
- * @LastEditTime: 2023-01-16 14:37:37
+ * @LastEditTime: 2023-01-17 21:16:49
  * @LastEditors: wsy
  */
 
@@ -22,20 +22,16 @@ export function greet(name: string | number) {
 type a = ReturnType<typeof greet>
 
 
-
-
-declare function handleRequest(method: "GET" | "POST"): void;
-
-
-interface Request {
-  url: string;
-  method: "GET" | "POST";
+interface Params {
+  url: string,
+  method: 'GET' | 'POST'
 }
 
-const req: Request = { url: "https://example.com", method: "GET" }
-const a = req.method
+declare function handleRequest({ url, method }: { url: string, method: 'GET' }): void;
 
-handleRequest(req.method);
+const req = { url: "https://example.com", method: "GET" } satisfies Params
+
+handleRequest(req); // OK!
 
 
 
@@ -59,3 +55,46 @@ myFavoriteNumber = 7;
 
 const arrayNumber: number[] = [1, 2, 3, 4];
 const greaterThan2: number = arrayNumber.find(num => num > 2) as number;
+
+
+type objType = {
+  readonly name: string,
+}
+
+const objTest: objType = {
+  name: 'wsy',
+}
+// Error 无法为“name”赋值，因为它是只读属性
+// @ts-expect-error
+objTest.name = 'wsy2'
+
+let str = 'wsy' // string
+
+
+let ss1 = {
+  name: 'wsy',
+  age: 18,
+} as const
+
+
+const ss2 = {
+  name: 'wsy',
+  age: 18,
+} as const
+
+
+type Union1 = string | number
+type Union2 = string | number | boolean
+
+// type isChildren = Union1 extends Union2 ? true : false
+
+type obj1 = {
+  name: string,
+}
+
+type obj2 = {
+  name: string,
+  age: number,
+}
+
+type isChildren = obj2 extends obj1 ? true : false
