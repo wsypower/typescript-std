490b51fa5e379b8b258b3a5e78d95e9c1f33e85f
weiyafei <2553241022@qq.com>
2023-01-17 17:17:06 +0800

    feat: function this

--- packages/basic/src/Everyday-Types.ts
+++ packages/basic/src/Everyday-Types.ts
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: wsy
  * @Date: 2023-01-16 12:59:55
- * @LastEditTime: 2023-01-17 16:37:53
+ * @LastEditTime: 2023-01-17 17:01:50
  * @LastEditors: wsy
  */
 
@@ -149,3 +149,35 @@ function myForEach(arr: any[], callback: (arg: any, index: number) => void) {
 myForEach([1, 2, 3], (a, i) => {
 
 });
+
+
+function makeDate(timestamp: number): Date;
+function makeDate(m: number, d: number, y: number): Date;
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
+  if (d !== undefined && y !== undefined) {
+    return new Date(y, mOrTimestamp, d);
+  } else {
+    return new Date(mOrTimestamp);
+  }
+}
+const d1 = makeDate(12345678);
+const d2 = makeDate(5, 5, 5);
+
+
+function len(s: string): number;
+function len(arr: any[]): number;
+function len(x: any) {
+  return x.length;
+}
+
+len(""); // OK
+len([0]); // OK
+
+
+const user = {
+  id: 123,
+  admin: false,
+  becomeAdmin: function () {
+    this.admin = true;
+  },
+};
