4cbb24eda9a39ef494026062911b46f708e30c16
weiyafei <2553241022@qq.com>
2023-01-26 20:46:37 +0800

    feat: extends

---
+++ packages/basic/src/extends.ts
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-01-26 20:43:27
+ * @LastEditTime: 2023-01-26 20:46:10
+ * @LastEditors: wsy
+ */
+interface a {
+  a: number;
+}
+
+interface ac extends a {
+  a: 1;
+}
+
+export { }
