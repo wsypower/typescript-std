5110acda2da9c6d700c0c04b73c7fa7629e91378
weiyafei <2553241022@qq.com>
2023-01-28 14:11:12 +0800

    feat: strictNullChecks

---
+++ packages/basic/src/strictNullChecks.ts
@@ -0,0 +1,47 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-01-28 13:13:36
+ * @LastEditTime: 2023-01-28 14:08:45
+ * @LastEditors: wsy
+ */
+/**
+* --strictNullChecks=false
+*/
+// let m1: boolean = undefined;
+// let m2: string = undefined;
+// let m3: number = undefined;
+// let m4: bigint = undefined;
+// let m5: symbol = undefined;
+// let m6: undefined = undefined;
+// let m7: null = undefined;
+
+// let n1: boolean = null;
+// let n2: string = null;
+// let n3: number = null;
+// let n4: bigint = null;
+// let n5: symbol = null;
+// let n6: undefined = null;
+// let n7: null = null;
+
+
+// let foo: string = undefined; // 正确，可以通过类型检查
+
+// foo.length;                  // 在运行时，将产生类型错误
+
+// function test(a: number, b?: number) {
+//   // Error “b”可能为“未定义”。ts(18048)
+//   return a + b
+// }
+
+// interface Test {
+//   a: number,
+//   b?: number //  Test.b?: number | undefined
+// }
+
+
+// type Test = {
+//   a: number,
+//   b?: number //  Test.b?: number | undefined
+// }
+export { }
