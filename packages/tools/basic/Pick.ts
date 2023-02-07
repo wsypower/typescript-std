007ad5e0f844e13a93cc5205e0333c67de6f05c0
weiyafei <2553241022@qq.com>
2023-02-07 20:20:35 +0800

    feat: Pick

---
+++ packages/tools/basic/Pick.ts
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-02-07 20:17:05
+ * @LastEditTime: 2023-02-07 20:17:13
+ * @LastEditors: wsy
+ */
+interface A {
+  x: number
+  y: number
+}
+type B = Pick<A, 'x'>
+
+
+
+export { }
