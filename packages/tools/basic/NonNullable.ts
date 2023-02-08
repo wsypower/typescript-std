dadd849cd149037badccab6f7e13cec1006f4db9
weiyafei <2553241022@qq.com>
2023-02-08 19:49:17 +0800

    feat: NonNullable

--- packages/tools/basic/NonNullable.ts
+++ packages/tools/basic/NonNullable.ts
@@ -2,9 +2,20 @@
  * @Description: 
  * @Author: wsy
  * @Date: 2023-02-08 19:03:11
- * @LastEditTime: 2023-02-08 19:03:49
+ * @LastEditTime: 2023-02-08 19:40:03
  * @LastEditors: wsy
  */
+type NonNullable<T> = T & {};
+
 type TO = NonNullable<string | number | undefined | null>
+
+type C = Object & null
+
+type A = null extends {} ? true : false
+// const a: A = 1
+
+type Alignment = string & {} | "left" | "center" | "right";
+
+
 
 export { }
