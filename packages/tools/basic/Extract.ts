b5263a9825ab1eacaa2be615bd45b20acc920f37
weiyafei <2553241022@qq.com>
2023-02-08 18:42:04 +0800

    feat: Extract

---
+++ packages/tools/basic/Extract.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-02-08 18:40:56
+ * @LastEditTime: 2023-02-08 18:41:01
+ * @LastEditors: wsy
+ */
+
+type a = 'a' | 'b' | 'c'
+type E = Extract<a, 'd' | 'c'>
+
+export { }
