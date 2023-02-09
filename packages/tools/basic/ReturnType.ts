c38b8808acd8f8999f721351f33b1a00bba12e12
weiyafei <2553241022@qq.com>
2023-02-09 20:37:34 +0800

    feat: ReturnType

---
+++ packages/tools/basic/ReturnType.ts
@@ -0,0 +1,13 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-02-09 20:36:43
+ * @LastEditTime: 2023-02-09 20:36:54
+ * @LastEditors: wsy
+ */
+
+type T0 = ReturnType<() => string>
+type T1 = ReturnType<() => () => {}>
+
+
+export { }
