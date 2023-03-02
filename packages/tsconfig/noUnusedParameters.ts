8614d7615d1041390d598033e9f45d34af79da2b
weiyafei <2553241022@qq.com>
2023-03-02 19:09:34 +0800

    noUnusedParameters

---
+++ packages/tsconfig/noUnusedParameters.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-03-02 19:08:31
+ * @LastEditTime: 2023-03-02 19:08:41
+ * @LastEditors: wsy
+ */
+function test(a: number, b: number) {
+  return a
+}
+
+export { }
