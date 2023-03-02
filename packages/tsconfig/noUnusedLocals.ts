8fd907d45795cc0be678c243d51f5b8963778702
weiyafei <2553241022@qq.com>
2023-03-02 19:08:05 +0800

    noUnusedLocals

---
+++ packages/tsconfig/noUnusedLocals.ts
@@ -0,0 +1,11 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-03-02 19:04:39
+ * @LastEditTime: 2023-03-02 19:04:59
+ * @LastEditors: wsy
+ */
+// @ts-expect-error
+const a = 1
+
+export { }
