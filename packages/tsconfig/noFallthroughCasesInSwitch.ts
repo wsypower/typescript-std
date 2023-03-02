1fc7707eef883e745b8099283fbf6fec9675461c
weiyafei <2553241022@qq.com>
2023-03-02 19:45:55 +0800

    noFallthroughCasesInSwitch

---
+++ packages/tsconfig/noFallthroughCasesInSwitch.ts
@@ -0,0 +1,22 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-03-02 19:39:31
+ * @LastEditTime: 2023-03-02 19:43:50
+ * @LastEditors: wsy
+ */
+
+const a: number = 0;
+
+switch (a) {
+  case 0:
+    console.log("zero");
+  case 1:
+    console.log("one");
+  case 2:
+    console.log("two");
+    break;
+}
+
+
+export { }
