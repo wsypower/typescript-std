7cc16a4ba7ef3579b3695556ae6fe31543a82c13
weiyafei <2553241022@qq.com>
2023-03-02 19:52:50 +0800

    noImplicitReturns

---
+++ packages/tsconfig/noImplicitReturns.ts
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-03-02 19:47:07
+ * @LastEditTime: 2023-03-02 19:50:22
+ * @LastEditors: wsy
+ */
+
+function handle(color: 'blue' | 'black') {
+  if (color === 'blue') {
+    return 'beats';
+  } else {
+    ('bose');
+  }
+}
