3120bbc17569b52171f39bfaebe2ac15888711c9
weiyafei <2553241022@qq.com>
2023-03-06 20:22:08 +0800

    allowUnreachableCode

---
+++ packages/tsconfig/allowUnreachableCode.ts
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-03-06 20:15:11
+ * @LastEditTime: 2023-03-06 20:15:15
+ * @LastEditors: wsy
+ */
+function foo() {
+  return 599;
+  console.log('Dead Code'); // Dead Code
+}
+
+foo()
+
+
+export { }
