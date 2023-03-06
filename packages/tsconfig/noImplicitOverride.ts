3a699dae09485f7a88571c463b84c89ed89bf27e
weiyafei <2553241022@qq.com>
2023-03-06 20:08:42 +0800

    noImplicitOverride

---
+++ packages/tsconfig/noImplicitOverride.ts
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-03-06 20:04:44
+ * @LastEditTime: 2023-03-06 20:05:13
+ * @LastEditors: wsy
+ */
+class Base {
+  print() { }
+}
+
+class Derived1 extends Base {
+  override print() {
+    // ...
+  }
+}
